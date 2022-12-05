71285558afbd6305c03b77dda22af88d2fa04663
Dark_Soul <irina2002.nikolaeva@yandex.ru>
2022-12-05 23:29:44 +0300

    refactor emul_page

--- emulation/emul_page.js
+++ emulation/emul_page.js
@@ -9,6 +9,7 @@ import plants from "./plants/index.js";
 import appliances from "./appliances/index.js";
 import checkConditions from "./emulation_modules/checkConditions.js";
 import changeEmulationTime from "./emulation_modules/changeEmulationTime.js";
+import growPlants from "./emulation_modules/growPlants.js";
 
 let greenhouseConditions = {
     Humidity: 60,
@@ -16,13 +17,6 @@ let greenhouseConditions = {
     Acidity: 7,
     LightLevel: 300,
 };
-
-//Nujno sozdat' setku/flexbox v teplitse 10x5 pod rasteniya i v kazdom bloke sdelat' fonom ochen' malen'kuyu ikonky rasteniya
-//Rasteniya hranyatsya v massive PLANTS
-//nuzen massiv blokov
-//v kazdom rastenii propisano ego mestopolozenie v teplitse, gde (0, 0) - leviy nizniy ugol setki (zdes' X i Y - nomer bloka setki)
-//nomer bloka v massive dolzen sootvetstvovat' nomeru ego rastenia v massive rasteniy
-//mozno v html, mozno zdes'
 
 //Nujno sozdat' bloki na kazdiy pribor, vse pribory hranyatsya v ob'ekte APPLIANCES v formate key(nazvanie): value(massiv etih priborov)
 //ne prinadlejat setke/flexu, (position relative ?)
@@ -34,42 +28,6 @@ let greenhouseConditions = {
 //potom na datchiki i pribory nalozhim eventListener, choby ih mozno bylo dvigat'
 
 //sozdat' 4 knopki vkl/vykl dlya kazdogo pribora
-
-const plantsBlocksArray = [];
-let tempPLantArray = [];
-for (let plant of document.querySelectorAll(".row img")) {
-    tempPLantArray.push(plant);
-    if (tempPLantArray.length == 10) {
-        plantsBlocksArray.unshift(...tempPLantArray);
-        tempPLantArray = [];
-    }
-}
-plantsBlocksArray.forEach((plant) => plant.setAttribute("width", "5px"));
-const col_width = document.querySelector(".col").offsetWidth;
-
-growPlants.growingDay = 6;
-function growPlants(currentDay) {
-    if (currentDay == 7)
-        plantsBlocksArray.forEach((plantBlock) =>
-            plantBlock.setAttribute("style", "visibility:visible")
-        );
-    if (currentDay >= 7 && currentDay != growPlants.growingDay) {
-        growPlants.growingDay = currentDay;
-        for (let plantNumber = 0; plantNumber < plants.length; plantNumber++) {
-            const plantBlockWidth = +plantsBlocksArray[plantNumber]
-                .getAttribute("width")
-                .slice(0, -2);
-            if (plantBlockWidth < 81) {
-                let plantGrowthRate = plants[plantNumber].getGrowthRate();
-
-                plantsBlocksArray[plantNumber].setAttribute(
-                    "width",
-                    `${plantBlockWidth + plantGrowthRate}px`
-                );
-            }
-        }
-    }
-}
 
 function showParameterValuesOnSensors() {
     for (let sensor in sensors) {
